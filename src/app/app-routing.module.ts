bbac06d46edefd6ac84deb1c47201d3df7415752
aakash1801 <singh.aakash1801@gmail.com>
2022-01-25 10:45:13 +0530

    add home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:'', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
-  //{path: 'signup', component: SignupComponent}
+  {path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    // HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-   // ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
